898e9af38ec88e8a56806137343b3f08eefe99f9
viktor-razroyev <razroyev@gmail.com>
2014-12-01 18:21:56 +0800

    8th

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function() {
     });
 
     function smooveInit() {
-        if ($('html').hasClass('ie')) {
+        if ($('html').hasClass('ie') || ifMobile()) {
             $(".webAppDevFlow").addClass('smoove-ready');
             $(".appearence_from_bottom, .aboutUs_item").css({visibility: 'visible'});
         } else {
@@ -55,5 +55,32 @@ jQuery(document).ready(function() {
                 cssClass: "smoove-ready"
             });
         }
+    }
+
+    function ifMobile () {
+        var $media_for_js = jQuery(".media_for_js"),
+            $ifMobileWidth = (parseInt($media_for_js.css("z-index")) > 960) ? false : true,
+            mobileUserAgent = {
+                Android: function() {
+                    return navigator.userAgent.match(/Android/i);
+                },
+                BlackBerry: function() {
+                    return navigator.userAgent.match(/BlackBerry/i);
+                },
+                iOS: function() {
+                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+                },
+                Opera: function() {
+                    return navigator.userAgent.match(/Opera Mini/i);
+                },
+                Windows: function() {
+                    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+                },
+                any: function() {
+                    return (mobileUserAgent.Android() || mobileUserAgent.BlackBerry() || mobileUserAgent.iOS() || mobileUserAgent.Opera() || mobileUserAgent.Windows());
+                }
+            };
+
+        return ($ifMobileWidth === mobileUserAgent.any()) ? true : false;
     }
 });
